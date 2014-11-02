43dc9073ffc2541aab94b85bcddde25459eb11b4
elfbandit <alouie789-github@yahoo.com>
2014-11-02 13:50:05 -0800

    Add email to exchange close
    Grabbing information from DB and sending an email to all participants
    when an exchange closes.

--- php/exchangeClose.php
+++ php/exchangeClose.php
@@ -11,22 +11,64 @@ if (ISSET($_POST['exchangeId'])) {
 		print_error("You do not have permission to close this exchange");
 		return;
 	}
+	
+	//Find all users affected by this closing:
+		$userList = query("SELECT u.userId, u.email
+						FROM exchangeUser e, user u
+						WHERE e.userId = u.userId
+						AND e.exchangeId =".$exchangeId);
 
+		$exchangeName = query("select exchangeName from exchange where exchangeId =".$exchangeId)->exchangeName;
+		$exchangeAdmin = mysql_fetch_assoc(query("select u.* from user u, exchangeUser e 
+												  WHERE u.userId = e.userId 
+												  AND e.exchangeId =".$exchangeId."
+												  AND e.permission = 3"));
+						
+	//Add all records to an array
+		$userIds = array();
+		$userEmails = array();
+		if(mysql_num_rows($userList) > 0){
+			while($row = mysql_fetch_array($userList))
+			{
+    			$userIds[] = $row["userId"];
+				$userEmails[] = $row["email"];
+			}
+		}
+		
 	//Find all gifts that are shared in the exchange, and mark them gifted
-		query("UPDATE gifts set gifted = true WHERE userId IN(
-						SELECT userId
-						FROM exchangeUser
-						WHERE exchangeId =".$exchangeId.")  
-					AND gifterId IN (
-						SELECT userId
-						FROM exchangeUser
-						WHERE exchangeId =".$exchangeId.")"
-					);
+		query("UPDATE gifts set gifted = true 
+				WHERE userId IN(". implode(",", $userIds).")
+				AND gifterId IN(". implode(",", $userIds).")");
+		if(mysql_errno() != 0){
+			print_error("Could not close exchange");
+			return;
+		}
+		
 	//Soft-delete the exchange
 		query("UPDATE exchange SET active=FALSE WHERE exchangeId='" . $exchangeId . "'");
 		if(mysql_errno() != 0){
 			print_error("Could not close exchange");
 		}else{
+			//Send an email to all affected users
+			$to = implode(",", $userEmails);
+			$subject = "Gifter: Exchange ".$exchangeName." is now closed";
+			$message = "<html><body>
+						Hello from Gifter! <br/> 
+						We just wanted to let you know that ".$exchangeName." is now closed and all gifts have been marked as gifted.
+						You can now take a look at your thank-you list to remember what you got. Check it out <a href = 'http://gifter.site.nfoservers.com/Gifter/main.php'>here</a> <br/>
+						<br />
+						Thanks!</br>
+						".$exchangeAdmin['firstName']."
+						</body></email>";
+			$headers = "From:".$exchangeAdmin['email']." \r\n" .
+			 			"Reply-To: ".$exchangeAdmin['email']." \r\n" .
+			 			"MIME-Version: 1.0" . "\r\n" .
+			 			"Content-type: text/html; charset=iso-8859-1" . "\r\n".
+    					"X-Mailer: PHP/" . phpversion();
+			mail($to, $subject, $message,$headers);
+				
+			
+			
 			print_success("Exchange Closed");	
 		}
 		
