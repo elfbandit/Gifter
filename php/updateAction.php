95c30f64a110450045f2640cbf46e9c2cc1e6df7
elfbandit <alouie789-github@yahoo.com>
2014-11-02 11:40:31 -0800

    Added session start to update

--- php/updateAction.php
+++ php/updateAction.php
@@ -1,5 +1,6 @@
 <?php
 include 'dbConnect.php';
+session_start();
 
 $jTableResult = array();
 
@@ -16,7 +17,7 @@ $value = mysql_real_escape_string($_POST["value"]);
 
 
 //Get records from database
-$result = mysql_query("UPDATE gifts SET name = '" . $name . "', link = '" . $link . "', description = '". $description ."',value = '". $value ."' WHERE giftId = " . $_POST["giftId"] . " AND userId=".$_SESSION["userInfo"]["userId"]);
+$result = mysql_query("UPDATE gifts SET name = '" . $name . "', link = '" . $link . "', description = '". $description ."',value = '". $value ."' WHERE giftId = " . $_POST["giftId"] . " AND userId = " . $_SESSION["userInfo"]["userId"]);
  if(mysql_error()){
 	echo mysql_error();
 }
